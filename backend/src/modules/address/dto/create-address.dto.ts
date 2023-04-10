52dbc5f264a33afda7bc73c2713c39bb6f24fcec
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-04-10 09:55:16 -0300

    feat: add dto address

--- backend/src/modules/address/dto/create-address.dto.ts
+++ backend/src/modules/address/dto/create-address.dto.ts
@@ -1,1 +1,27 @@
-export class CreateAddressDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateAddressDto {
+  @IsString()
+  @IsNotEmpty()
+  cep: string;
+
+  @IsString()
+  @IsNotEmpty()
+  state: string;
+
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @IsString()
+  @IsNotEmpty()
+  street: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  number: number;
+
+  @IsString()
+  @IsNotEmpty()
+  complement: string;
+}
