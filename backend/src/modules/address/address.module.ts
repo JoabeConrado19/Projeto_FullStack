728140a37ddbd9727c0d5fa4ce1221844b4f7d0d
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-04-10 10:30:37 -0300

    feat: add address modules

--- backend/src/modules/address/address.module.ts
+++ backend/src/modules/address/address.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AddressRepository } from './_repositories/address.repository';
+import { AddressPrismaRepository } from './_repositories/prisma/address.prisma.repository';
 
 @Module({
   controllers: [AddressController],
-  providers: [AddressService],
+  providers: [
+    AddressService,
+    PrismaService,
+    {
+      provide: AddressRepository,
+      useClass: AddressPrismaRepository,
+    },
+  ],
+  exports: [AddressService],
 })
 export class AddressModule {}
