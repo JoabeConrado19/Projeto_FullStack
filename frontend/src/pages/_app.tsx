0f4ffd710960ba1d6dfcf31d2b97d0546f19bf65
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2023-04-14 09:13:51 -0300

    feat: custom button themes provider done

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,14 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import GlobalStyle from "@/styles/stylesGlobal";
+import "../styles/resetStyles.css"
+import "../styles/globalStyles.css"
+import { ThemeProvider } from "@emotion/react";
+
+import theme from "@/styles/customMaterialUIThemes";
 
 export default function App({ Component, pageProps }: AppProps) {
  return (
-  <>
-   <GlobalStyle />
+  <ThemeProvider theme={theme}>
    <Component {...pageProps} />
-  </>
+  </ThemeProvider>
  );
 }
