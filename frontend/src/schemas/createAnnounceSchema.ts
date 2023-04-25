e893ef69522be523d995d293bb8e2a4c3b4a24bf
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2023-04-24 22:52:39 -0300

    fix: createAnnounceSchema fixed

--- frontend/src/schemas/createAnnounceSchema.ts
+++ frontend/src/schemas/createAnnounceSchema.ts
@@ -1,4 +1,5 @@
 import * as yup from "yup";
+import {Schema} from "yup"
 
 interface ICreateAnnounceSchema {
     brand: string
@@ -7,27 +8,27 @@ interface ICreateAnnounceSchema {
     fuelType: string
     miles: string
     color: string
-    price: number
+    price: string
     description: string
-    images_url: string[]
+    images_url: IImageUrl[]
 }
 
 interface IImageUrl {
     url: string
 }
 
-const imagesUrlSchema = yup.object().shape({
+const imagesUrlSchema: Schema<IImageUrl> = yup.object().shape({
     url: yup.string().required("Insira um link")
 })
 
-export const formCreateAnnounceSchema = yup.object().shape({
+export const formCreateAnnounceSchema = yup.object({
     brand: yup.string().required("Seleciona uma marca"),
-    model: yup.string().required("Seleciona um modelo"),
-    year: yup.string().required("Seleciona um ano"),
-    fuelType: yup.string().required("Seleciona o combustivel"),
+    model: yup.string().required("Selecione um modelo"),
+    year: yup.string().required("Selecione um ano"),
+    fuelType: yup.string().required("Selecione o combustivel"),
     miles: yup.string().required("Informe a quilometragem"),
     color: yup.string().required("Informe a cor"),
     price: yup.string().required("Informe o preço"),
     description: yup.string().required("Informe uma descrição"),
-    images_url: yup.array().of(imagesUrlSchema)
+    images_url: yup.array().of(imagesUrlSchema).required("Informe link para as imagens")
 })
