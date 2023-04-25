48e6230b7104a524c2090565279347cba5b850eb
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2023-04-24 22:51:22 -0300

    chore: optimizing button component

--- frontend/src/components/Buttons/index.tsx
+++ frontend/src/components/Buttons/index.tsx
@@ -1,3 +1,6 @@
+import {
+    ButtonHTMLAttributes
+  } from "react";
 import styled from "./styles.module.css"
 interface IProps {
     children: string
@@ -11,5 +14,15 @@ export const ButtonOne = ({children}:IProps ) => {
 export const ButtonTwo = ({children}:IProps ) => {
     return (
         <button className={styled.registerButton}>{children}</button>
+    )
+}
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode
+}
+
+export const ButtonComponent = ({children, ...rest}: IButton) => {
+    return (
+        <button {...rest} className={` ${styled.button_base} ${rest.className ? rest.className : ""}`}>{children}</button>
     )
 }
