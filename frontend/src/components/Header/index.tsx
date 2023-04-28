b9c97a90b9a5c904566f09f60cc40e516d954cdb
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2023-04-28 09:08:08 -0300

    feat: desktop/mobile menu done

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import style from "./style.module.css";
-import buttonStyle from "@/components/Buttons/styles.module.css"
+import buttonStyle from "@/components/Buttons/styles.module.css";
 
 import { Button } from "@mui/material";
 
@@ -18,6 +18,9 @@ import { useRouter } from "next/router";
 export default function HeaderComponent() {
   const [userData, setUserData] = useState<null | IUserData>(null);
 
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+  const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false);
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   const [showEditModal, setShowEditModal] = useState<boolean>(false);
 
   const router = useRouter();
@@ -40,73 +43,149 @@ export default function HeaderComponent() {
     };
 
     getUser();
+
+    window.addEventListener("resize", () => {
+      if (window.innerWidth < 700) {
+        setShowMobileMenu(true);
+      } else {
+        setShowMobileMenu(false);
+      }
+    });
   }, []);
 
   return (
     <header className={style.page_header}>
       <LogoComponent />
-      <div className={style.page_header_buttons}>
-        {userData ? (
-          <div className={style.profileContainer}>
-            <div className={style.profileImageContainer}>
-              <p>
-                {userData.name.split(" ").reduce((acc, act) => {
-                  acc += act[0].toLocaleUpperCase();
 
-                  return acc;
-                }, "")}
-              </p>
-            </div>
-            <p>{userData.name}</p>
-            <ul className={style.hidden_menu}>
-              <li>
-                <ButtonComponent
-                  onClick={() => {
-                    setShowEditModal(true);
-                  }}
-                  className={buttonStyle.hidden_menu_buttons}
-                >
-                  Editar usuário
-                </ButtonComponent>
-              </li>
-              <li>
-                <ButtonComponent className={buttonStyle.hidden_menu_buttons}>
-                  Editar endereço
-                </ButtonComponent>
-              </li>
-              {userData.accountType === "Anunciante" ? (
-                <li>
-                  <a href="/announcement">
-                    <ButtonComponent className={buttonStyle.hidden_menu_buttons}>Meus anúncios</ButtonComponent>
-                  </a>
-                </li>
-              ) : null}
-              <li>
-                <ButtonComponent
-                  className={buttonStyle.hidden_menu_buttons}
-                  onClick={() => {
-                    destroyCookie(undefined, "tokenMotorsShop");
-
-                    router.push("/login");
-                  }}
-                >
-                  Sair
-                </ButtonComponent>
-              </li>
-            </ul>
-          </div>
-        ) : (
-          <>
-            <Button>
-              <Link href="/login">Fazer login</Link>
-            </Button>
-            <Button variant="outlined">
-              <Link href="/register">Cadastrar</Link>
-            </Button>
-          </>
-        )}
+      <div className={style.mobile_menu_button}>
+        <ButtonComponent
+          onClick={() => {
+            setShowMobileMenu((prevState) => !prevState);
+          }}
+          className={`${buttonStyle.no_style_button}`}
+        >
+          X
+        </ButtonComponent>
       </div>
+      <div
+        className={style.page_header_menu}
+        style={{
+          display: `${showMobileMenu ? "none" : "flex"}`,
+        }}
+      >
+        <ul className={style.mobile_menu}>
+          <li>
+            <Link href="/">
+              <ButtonComponent className={`${buttonStyle.no_style_button}`}>
+                Carros
+              </ButtonComponent>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <ButtonComponent className={`${buttonStyle.no_style_button}`}>
+                Motos
+              </ButtonComponent>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <ButtonComponent className={`${buttonStyle.no_style_button}`}>
+                Leilão
+              </ButtonComponent>
+            </Link>
+          </li>
+        </ul>
+        <div className={style.page_header_buttons}>
+          {userData ? (
+            <div className={style.profile_container}>
+              <ButtonComponent
+                className={buttonStyle.no_style_button}
+                onClick={() => {
+                  setShowMenu((prevState) => !prevState);
+                }}
+              >
+                <div className={style.profile_infos}>
+                  <div
+                    className={style.profile_image_container}
+                    style={{ backgroundColor: `${userData.color}` }}
+                  >
+                    <p>
+                      {userData.name.split(" ").reduce((acc, act) => {
+                        acc += act[0].toLocaleUpperCase();
 
+                        return acc;
+                      }, "")}
+                    </p>
+                  </div>
+                  <p>{userData.name}</p>
+                </div>
+              </ButtonComponent>
+              <ul
+                className={style.hidden_menu}
+                style={{
+                  display: `${showMenu ? "unset" : "none"}`,
+                }}
+              >
+                <li>
+                  <ButtonComponent
+                    onClick={() => {
+                      setShowEditModal(true);
+                    }}
+                    className={buttonStyle.hidden_menu_buttons}
+                  >
+                    Editar usuário
+                  </ButtonComponent>
+                </li>
+                <li>
+                  <ButtonComponent className={buttonStyle.hidden_menu_buttons}>
+                    Editar endereço
+                  </ButtonComponent>
+                </li>
+                {userData.accountType === "Anunciante" ? (
+                  <li>
+                    <a href="/announcement">
+                      <ButtonComponent
+                        className={buttonStyle.hidden_menu_buttons}
+                      >
+                        Meus anúncios
+                      </ButtonComponent>
+                    </a>
+                  </li>
+                ) : null}
+                <li>
+                  <ButtonComponent
+                    className={buttonStyle.hidden_menu_buttons}
+                    onClick={() => {
+                      destroyCookie(undefined, "tokenMotorsShop");
+
+                      router.push("/login");
+                    }}
+                  >
+                    Sair
+                  </ButtonComponent>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <>
+              <Link href="/login">
+                <ButtonComponent
+                  className={buttonStyle.no_style_button}
+                  style={{ color: "var(--brand-1)" }}
+                >
+                  Fazer login
+                </ButtonComponent>
+              </Link>
+              <Link href="/register">
+                <ButtonComponent className={buttonStyle.none_black_button}>
+                  Cadastrar
+                </ButtonComponent>
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
       {showEditModal ? (
         <EditUserInfosModal
           closeModalFunc={setShowEditModal}
