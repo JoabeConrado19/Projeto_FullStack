6b5ea716686564dbf0ed259352db44b08f67b131
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2023-04-28 09:13:37 -0300

    feat: new modals and small alteration in ModalBase

--- frontend/src/components/Modals/EditUserInfosModal/index.tsx
+++ frontend/src/components/Modals/EditUserInfosModal/index.tsx
@@ -1,12 +1,17 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import ModalBase from "../ModalBase";
-import { InputOne, TextAreaInput } from "@/components/Input/modalInputs";
+import {
+  InputComponent,
+  TextAreaInputComponent,
+} from "@/components/Input/modalInputs";
+import buttonStyle from "@/components/Buttons/styles.module.css";
+import style from "./style.module.css";
 import { useForm } from "react-hook-form";
 
 import { ButtonComponent } from "@/components/Buttons";
-import buttonStyle from "@/components/Buttons/styles.module.css";
 import { IUserData } from "@/interfaces/user";
 import api from "@/services/api";
+import DeleteUserConfirmModal from "../DeleteUserConfirmModal";
 
 interface IEditUserInfosModal {
   closeModalFunc: Dispatch<SetStateAction<boolean>>;
@@ -17,88 +22,98 @@ export default function EditUserInfosModal({
   closeModalFunc,
   userData,
 }: IEditUserInfosModal) {
+  const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
+
   const { register, handleSubmit } = useForm();
 
   const editUserRequest = (data: any) => {
-    const {
-      address,
-      cars,
-      comments,
-      accountType,
-      createdAt,
-      id,
-      profileImage,
-      ...filteredUserData
-    } = userData;
+    const { id } = userData;
 
-    const updatedData = {
-      ...filteredUserData,
-      ...data,
-    };
+    api.patch(`/users/${id}`, data);
 
-    api.patch(`/users/${id}`, data)
-
-    location.reload()
+    location.reload();
   };
 
   return (
-    <ModalBase modalTitle="Editar perfil" closeModal={closeModalFunc}>
-      <form onSubmit={handleSubmit(editUserRequest)}>
-        <p className="headline-7-600">Informações pessoais</p>
-        <InputOne
-          inputId="user_name"
-          label="Nome"
-          register={register("name")}
-          defaultValue={userData.name}
+    <>
+      <ModalBase modalTitle="Editar perfil" closeModal={closeModalFunc}>
+        <form onSubmit={handleSubmit(editUserRequest)}>
+          <p className="headline-7-600">Informações pessoais</p>
+          <InputComponent
+            inputId="user_name"
+            label="Nome"
+            register={register("name")}
+            defaultValue={userData.name}
+          />
+          <InputComponent
+            inputId="user_email"
+            label="Email"
+            register={register("email")}
+            defaultValue={userData.email}
+          />
+          <InputComponent
+            inputId="user_cpf"
+            label="CPF"
+            register={register("cpf")}
+            defaultValue={userData.cpf}
+          />
+          <InputComponent
+            inputId="user_phone"
+            label="Celular"
+            register={register("phone")}
+            defaultValue={userData.phone}
+          />
+          <InputComponent
+            inputId="user_birthdate"
+            label="Data de nascimento"
+            register={register("birthdate")}
+            defaultValue={userData.birthdate}
+          />
+          <TextAreaInputComponent
+            inputId="user_description"
+            label="Descrição"
+            register={register("description")}
+            defaultValue={userData.description}
+          />
+          <div className={style.buttons_container}>
+            <div className={style.delete_button_container}>
+              <ButtonComponent
+                type="button"
+                className={buttonStyle.alert1_white_button}
+                onClick={() => {
+                  setShowDeleteModal(true)
+                }}
+              >
+                Deletar usuário
+              </ButtonComponent>
+            </div>
+            <div className={style.confirm_cancel_button_container}>
+              <ButtonComponent
+                onClick={() => {
+                  closeModalFunc((prevState) => {
+                    return !prevState;
+                  });
+                }}
+                type="button"
+              >
+                Cancelar
+              </ButtonComponent>
+              <ButtonComponent
+                type="submit"
+                className={buttonStyle.brand1_white_button}
+              >
+                Salvar alterações
+              </ButtonComponent>
+            </div>
+          </div>
+        </form>
+      </ModalBase>
+      {showDeleteModal ? (
+        <DeleteUserConfirmModal
+          closeModalFunc={setShowDeleteModal}
+          userData={userData}
         />
-        <InputOne
-          inputId="user_email"
-          label="Email"
-          register={register("email")}
-          defaultValue={userData.email}
-        />
-        <InputOne
-          inputId="user_cpf"
-          label="CPF"
-          register={register("cpf")}
-          defaultValue={userData.cpf}
-        />
-        <InputOne
-          inputId="user_phone"
-          label="Celular"
-          register={register("phone")}
-          defaultValue={userData.phone}
-        />
-        <InputOne
-          inputId="user_birthdate"
-          label="Data de nascimento"
-          register={register("birthdate")}
-          defaultValue={userData.birthdate}
-        />
-        <TextAreaInput
-          inputId="user_description"
-          label="Descrição"
-          register={register("description")}
-          defaultValue={userData.description}
-        />
-        <div>
-          <ButtonComponent
-            onClick={() => {
-              closeModalFunc((prevState) => {
-                return !prevState;
-              });
-            }}
-          >
-            Cancelar
-          </ButtonComponent>
-          <ButtonComponent
-            type="submit"
-            className={buttonStyle.brand1_white_button}
-          >
-            Salvar alterações
-          </ButtonComponent>
-        </div>
-      </form>
-    </ModalBase>
+      ) : null}
+    </>
   );
 }
