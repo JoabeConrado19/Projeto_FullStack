39b4b22d3f14716cb3b334103e9bcc4ec0fec0df
juliomello93 <juliomello@live.com>
2023-05-11 13:11:27 -0300

    fix:dependencies

--- frontend/src/context/AnnouncementPageContext.tsx
+++ frontend/src/context/AnnouncementPageContext.tsx
@@ -1,85 +1,85 @@
-import {
- createContext,
- useEffect,
- useState,
- Dispatch,
- SetStateAction,
-} from "react";
-import { IProviderProps } from "./RegisterLoginContext";
-import { destroyCookie, parseCookies } from "nookies";
-import api from "@/services/api";
 import { ICarsData } from "@/interfaces/announcement";
 import { IUserData } from "@/interfaces/user";
-import { NextRouter, useRouter } from "next/router";
+import api from "@/services/api";
 import { parseJwt } from "@/utils/jwt";
+import { NextRouter, useRouter } from "next/router";
+import { destroyCookie, parseCookies } from "nookies";
+import {
+    Dispatch,
+    SetStateAction,
+    createContext,
+    useEffect,
+    useState,
+} from "react";
+import { IProviderProps } from "./RegisterLoginContext";
 export const announcementPage = createContext({} as IAnnouncementProviderData);
 
 interface IAnnouncementProviderData {
- userAnnouncements: [] | ICarsData[];
- user: IUserData | null;
- setUser: Dispatch<SetStateAction<IUserData | null>>;
- router: NextRouter;
- carUser: string;
- userCarData: IUserData | null;
+    userAnnouncements: [] | ICarsData[];
+    user: IUserData | null;
+    setUser: Dispatch<SetStateAction<IUserData | null>>;
+    router: NextRouter;
+    carUser: string;
+    userCarData: IUserData | null;
 }
 
 export const AnnouncementPageProvider = ({ children }: IProviderProps) => {
- const [userAnnouncements, setUserAnnouncements] = useState([]);
- const [user, setUser] = useState<IUserData | null>(null);
- const [carUser, setCarUser] = useState<string>("");
- const [userCarData, setUserCarData] = useState<IUserData | null>(null);
- const router = useRouter();
+    const [userAnnouncements, setUserAnnouncements] = useState([]);
+    const [user, setUser] = useState<IUserData | null>(null);
+    const [carUser, setCarUser] = useState<string>("");
+    const [userCarData, setUserCarData] = useState<IUserData | null>(null);
+    const router = useRouter();
 
- useEffect(() => {
-  const getUser = async () => {
-   const userToken = parseCookies().tokenMotorsShop;
-   if (userToken) {
-    try {
-     api.defaults.headers.common.Authorization = `Bearer ${userToken}`;
+    useEffect(() => {
+        const getUser = async () => {
+            const userToken = parseCookies().tokenMotorsShop;
+            if (userToken) {
+                try {
+                    api.defaults.headers.common.Authorization = `Bearer ${userToken}`;
 
-     const tokenUserData = parseJwt(userToken);
+                    const tokenUserData = parseJwt(userToken);
 
-     const { data }: { data: any } = await api.get(
-      `/users/${tokenUserData.sub}`
-     );
-     setUser(data);
-    } catch {
-     destroyCookie(undefined, "tokenMotorsShop");
+                    const { data }: { data: any } = await api.get(
+                        `/users/${tokenUserData.sub}`
+                    );
+                    setUser(data);
+                } catch {
+                    destroyCookie(undefined, "tokenMotorsShop");
 
-     router.push("/login");
-    }
-   }
-  };
+                    router.push("/login");
+                }
+            }
+        };
 
-  const getAnnouncement = async () => {
-   const userAnnouncement = router.query;
-   try {
-    const { data }: { data: any } = await api.get(
-     `/users/${userAnnouncement.id}`
+        const getAnnouncement = async () => {
+            const userAnnouncement = router.query;
+            try {
+                const { data }: { data: any } = await api.get(
+                    `/users/${userAnnouncement.id}`
+                );
+                setCarUser(data.id);
+                setUserAnnouncements(data.cars);
+                setUserCarData(data);
+                console.log(data)
+            } catch { }
+        };
+
+        getUser();
+        getAnnouncement();
+    }, [router, user]);
+
+    return (
+        <announcementPage.Provider
+            value={{
+                userAnnouncements,
+                userCarData,
+                user,
+                setUser,
+                router,
+                carUser,
+            }}
+        >
+            {children}
+        </announcementPage.Provider>
     );
-    setCarUser(data.id);
-    setUserAnnouncements(data.cars);
-    setUserCarData(data);
-    console.log(data)
-   } catch {}
-  };
-
-  getUser();
-  getAnnouncement();
- }, [router]);
-
- return (
-  <announcementPage.Provider
-   value={{
-    userAnnouncements,
-    userCarData,
-    user,
-    setUser,
-    router,
-    carUser,
-   }}
-  >
-   {children}
-  </announcementPage.Provider>
- );
 };
